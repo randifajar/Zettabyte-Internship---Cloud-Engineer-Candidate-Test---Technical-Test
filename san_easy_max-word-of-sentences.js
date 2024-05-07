47bb79defac5272f54e1217cf146ae4a15b1acf4
Randi Fajar Wicaksono <46032161+randifajar@users.noreply.github.com>
2024-05-07 14:31:33 +0700

    Add javascript files

---
+++ san_easy_max-word-of-sentences.js
@@ -0,0 +1,23 @@
+/**
+ * Direction:
+ * Find maximum number of words from given sentences.
+ *
+ * Expected:
+ * 23
+ */
+const sentences = [
+  'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+  'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+  'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+];
+
+function result(sentences) {
+  const len_array = [];
+  for (i in sentences) {
+    const result = sentences[i].trim().split(/\s+/);
+    len_array.push(result.length);
+  }
+  return Math.max(...len_array);
+}
+
+console.log(result(sentences));
